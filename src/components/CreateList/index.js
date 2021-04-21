7daeab0cfbb71a796f4162e5c97fc7f0033cc99f
Your Name <you@example.com>
2021-04-21 14:43:05 +0200

    I passed search component props at createList component. I get the town value in search but undifined in createList

--- src/components/CreateList/index.js
+++ src/components/CreateList/index.js
@@ -1,5 +1,5 @@
 // NPM imports
-import React from 'react';
+import React, { useState } from 'react';
 
 // Components imports
 import Search from '../Search';
@@ -7,13 +7,36 @@ import Search from '../Search';
 // Styles imports
 import './createList.css';
 
-function CreateList () {
+function CreateList ({ onChange }, queryValue) {
+
+    const [ title, setTitle ] = useState('')
+
+    const handleGetTitle = (e, func) => {
+        func(e.target.value)
+    }
 
     const handleSaveList = (e) => {
         e.preventDefault()
-        console.log('je suis la')
-    }
+ 
+        // je récupère la div container
+        const docTitle = document.querySelector('.list-result')
 
+        // je récupère la valeur de mon input title
+        const valueTitle = document.querySelector('.title-input').value
+
+        // je crée un nouvel élèment "p"
+        const p = document.createElement('p')
+
+        // ajouter une className au titre pour le personnaliser
+        p.classList = 'title-result'
+
+        // j ajoute le texte à la balise p
+        p.textContent = valueTitle
+
+        // j'ajoute mon nouvel élèment en enfant.
+        docTitle.appendChild(p) 
+        console.log(queryValue, 'createList')      
+    }
 
     return(
         <div className="createList-content">
@@ -21,12 +44,13 @@ function CreateList () {
             <form className="add-town-content">
                 <div className="create-list">
                     <label className="title-label">Nom de la zone</label>
-                    <input className="title-input" placeholder="Nom"></input>
+                    <input className="title-input" placeholder="Nom" value={title} onChange={(e) => handleGetTitle(e, setTitle, 'title')}></input>
                 </div>
-                <Search />
-                <button className="save-button" onSubmit={(e) => handleSaveList}>Sauvegarder</button>
+                <Search className='search-input' onChange={onChange} />
+                <img></img>
+                <button className="save-button" onClick={handleSaveList}>Sauvegarder</button>
             </form>
-         
+            <div className="list-result__empty list-result"></div>
         </div>
     )
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -10,114 +10,108 @@ import './search.css';
 
 
 const PER_PAGE = 50;
-const SEARCH_URI = 'https://api.github.com/search/users';
+// const SEARCH_URI = 'https://geo.api.gouv.fr/communes?nom=Nantes&fields=departement&boost=population&limit=5';
 
 
-function makeAndHandleRequest(query, page = 1) {
-  console.log("query");
-  return fetch(`${SEARCH_URI}?q=${query}+in:login&page=${page}&per_page=50`)
-    .then(resp => resp.json())
-    .then(({ items, total_count }) => {
-      console.log(items);
-      const options = items.map(i => ({
-        avatar_url: i.avatar_url,
-        id: i.id,
-        login: i.login,
-      }));
-      return { options, total_count };
-    });
-}
 
 function Search () {
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [options, setOptions] = useState([])
+  const [query, setQuery] = useState('')
+
+  let _cache = {};
     
-    const [isLoading, setIsLoading] = useState(false)
-    const [options, setOptions] = useState([])
-    const [query, setQuery] = useState('')
-    let _cache = {};
-
-
-    const _handleInputChange = (query) => {
+  const  makeAndHandleRequest = (query, page = 1) =>{
     console.log(query);
-    setQuery(query)
-    };
+    return fetch(`https://geo.api.gouv.fr/communes?nom=${query}&fields=communes`)
+      .then(async (items) => {
+        let list = await items.json()
+        /* eslint-disable */ 
+        let options =  list.map(i =>
+        ({ nom: i.nom}),
+        );
+        return {options: options, total_count: options.length}
+      });
+  }
+
+  // const _handleInputChange = (query) => {
+  // setQuery(query)
+  // };
 
     
 
-    const _handlePagination = (e, shownResults) => {
-        const cachedQuery = _cache[query];
-        // console.log("ckjdshbcyu");
+  const _handlePagination = (e, shownResults) => {
+      const cachedQuery = _cache[query];
 
+      if (
+      cachedQuery.options.length > shownResults ||
+      cachedQuery.options.length === cachedQuery.total_count
+      ) {
+      return;
+      }
 
-        if (
-        cachedQuery.options.length > shownResults ||
-        cachedQuery.options.length === cachedQuery.total_count
-        ) {
+      setIsLoading( true);
+
+      const page = cachedQuery.page + 1;
+
+      makeAndHandleRequest(query, page).then(resp => {
+      const options = cachedQuery.options.concat(resp.options);
+      _cache[query] = { ...cachedQuery, options, page };
+      setIsLoading(false)
+      setOptions(options)
+      });
+  };
+
+  const _handleSearch = query => {
+      if (_cache[query]) {
+        setOptions( _cache[query].options );
         return;
-        }
-
-        setIsLoading( true);
-
-        const page = cachedQuery.page + 1;
-
-        makeAndHandleRequest(query, page).then(resp => {
-        const options = cachedQuery.options.concat(resp.options);
-        _cache[query] = { ...cachedQuery, options, page };
+      }
+  
+      setIsLoading(true)
+      makeAndHandleRequest(query).then(resp => {
+        _cache[query] = { ...resp, page: 1 };
         setIsLoading(false)
-        setOptions(options)
-        });
-    };
+        setOptions(resp.options)
+      });
+  };
 
-    const _handleSearch = query => {
-        // console.log("oiugb");
-        // console.log(query);
-        if (_cache[query]) {
-          setOptions( _cache[query].options );
-          return;
-        }
-    
-        setIsLoading(true)
-        makeAndHandleRequest(query).then(resp => {
-          _cache[query] = { ...resp, page: 1 };
-          setIsLoading(false)
-          setOptions(resp.options)
-        });
-    };
-    
-    return(
-        <div className="search-content">
-    
-            <label className="search-label">Ville</label>
-            <AsyncTypeahead
-                className="search-input"
-                isLoading={isLoading}
-                options={options}
-                query={query}
-                id="async-pagination-example"
-                labelKey="login"
-                maxResults={PER_PAGE - 1}
-                minLength={2}
-                onInputChange={_handleInputChange}
-                onPaginate={_handlePagination}
-                onSearch={_handleSearch}
-                paginate
-                placeholder="Search for a Github user..."
-                renderMenuItemChildren={option => (
-                    <div key={option.id}>
-                    <img
-                        alt={option.login}
-                        src={option.avatar_url}
-                        style={{
-                        height: '24px',
-                        marginRight: '10px',
-                        width: '24px',
-                        }}
-                    />
-                    <span>{option.login}</span>
-                    </div>
-                )}
-                useCache={false}
-            />
+
+  const handleGetValue = (query) => {
+    console.log(query[0].nom, "ola");
+    const queryValue = query[0].nom;
+    setQuery(queryValue);
+
+    console.log(queryValue, "yes");
+  };
+
+  return(
+      <div className="search-content">
+          <label className="search-label">Ville</label>
+          <AsyncTypeahead
+              className="search-input"
+              isLoading={isLoading}
+              options={options}
+              query={query}
+              onChange={(e) => handleGetValue(e, setQuery, "queryValue")}
+              id="async-pagination-example"
+              labelKey="nom"
+              maxResults={PER_PAGE - 1}
+              minLength={2}
+              // onInputChange={_handleInputChange}
+              onPaginate={_handlePagination}
+              onSearch={_handleSearch}
+              paginate
+              placeholder="Recherchez une ville ..."
+              renderMenuItemChildren={(option, i) => (
+                  <div key={i}>
+                    <span>{option.nom}</span>
+                  </div>
+              )}
+              useCache={false}
+              highlightOnlyResult="true"
+          />
     </div>
   )
 }
